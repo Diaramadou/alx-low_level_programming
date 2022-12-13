b4dab29f9fb9195f36ea63506a50f74f78a3461a
Diaramadou <couda.dm@gmail.com>
2022-12-12 18:16:07 -0800

    demo

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+
+int main(void)
+
+{
+	int n ;
+	srand(time(0)) ;
+	n = rand() - RAND_MAX / 2 ;
+	if (n < 0)
+		return ("is negative") ;
+	if (n > 0)
+		return ("is positive") ;
+	if (n = 0)
+		return ("is zero") ;
+	return (0) ;
+
+}
+	
