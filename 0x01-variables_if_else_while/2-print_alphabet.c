f21d0eae69a205aebb8398da1449fbc5f1dec042
Diaramadou <couda.dm@gmail.com>
2022-12-17 14:24:08 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
